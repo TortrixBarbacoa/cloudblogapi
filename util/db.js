9e53245cbd01aa2e17dc23b46693b951d792c5fc
Elian Alvarado <elian.alvarado.2004@gmail.com>
2024-11-22 01:26:41 -0600

    corrección 2

--- util/db.js
+++ util/db.js
@@ -2,37 +2,35 @@ const mysql = require('mysql2');
 const AWS = require('aws-sdk');
 const config = require('../config/config.json');
 
-// Initialize AWS RDS client
-const rds = new AWS.RDS();
 
-// Function to generate the IAM authentication token
 function generateAuthToken() {
   return new Promise((resolve, reject) => {
-    const token = rds.generateDbAuthToken({
-      DBHostname: config.host,
-      Port: 3306,
-      DBUsername: config.user
+    const signer = new AWS.RDS.Signer({
+      region: config.region, 
+      hostname: config.host,  
+      port: 3306,  
+      username: config.user,  
     });
-    resolve(token); // Return the generated token
+
+
+    const token = signer.getAuthToken();
+    resolve(token);
   });
 }
 
-// Create a MySQL connection pool
 async function createPool() {
   try {
-    const password = await generateAuthToken(); // Generate IAM token for password
+    const password = await generateAuthToken(); 
 
     const pool = mysql.createPool({
       host: config.host,
       user: config.user,
       database: config.database,
       password: password,
-      ssl: {
-        ca: '/path/to/your/rds-ca-cert.pem'
-      }
+      
     });
 
-    return pool.promise(); // Return pool with promise API
+    return pool.promise();
   } catch (error) {
     console.error('Error generating DB auth token or creating pool:', error);
     throw error;
